15b000687eeab627c487e785b3b42a165ddfd30c
Jimmuel Sanggayan <jimmuelsanggayan@gmail.com>
2026-05-10 20:22:24 +0800

    vaa

--- .railway/start.sh
+++ .railway/start.sh
@@ -3,6 +3,9 @@
 export APP_ENV=production
 export APP_DEBUG=false
 
+composer install --no-dev --optimize-autoloader
+npm install
+npm run build
 php artisan storage:link
 php artisan config:cache
 php artisan route:cache
